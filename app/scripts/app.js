900a8792183f1b801071df9ceb0ab8313c0558fc
Colin Carter <colincarter@me.com>
2015-01-24 21:27:19 -0600

    Put link content into separate relative page includes.  Adjust JS animations.  Configure hero/splash interaction.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,42 @@
 // Show/Hide page sections
-$(document).ready(function() {
-  $('#hero').hide();
-  $('#about').hide();
-  $('#contact').hide();
+$('.js-about').hide();
+$('.js-blog').hide();
+$('.js-contact').hide();
+$('.js-tour').hide();
+
+$('.js-hero').click(function() {
+  $('.js-header').css("display", "block");
+  $('.js-header').hide();
+  $('.js-content-primary').css("display", "block");
+  $('.js-hero').hide(1000);
+  $('.js-header').fadeIn(2000);
+  $('.js-about').show(1000);
 });
 
 $('.js-link-about').click(function() {
-  $('#about').hide();
+  $('.js-about').show(1000);
+  $('.js-blog').hide(1000);
+  $('.js-contact').hide(1000);
+  $('.js-tour').hide(1000);
+});
+
+$('.js-link-blog').click(function() {
+  $('.js-about').hide(1000);
+  $('.js-blog').show(1000);
+  $('.js-contact').hide(1000);
+  $('.js-tour').hide(1000);
+});
+
+$('.js-link-contact').click(function() {
+  $('.js-about').hide(1000);
+  $('.js-blog').hide(1000);
+  $('.js-contact').show(1000);
+  $('.js-tour').hide(1000);
+});
+
+$('.js-link-tour').click(function() {
+  $('.js-about').hide(1000);
+  $('.js-blog').hide(1000);
+  $('.js-contact').hide(1000);
+  $('.js-tour').show(1000);
 });
