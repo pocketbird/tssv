25ed9a1d9fb50d039423e0aece49582e3f3404a4
Colin Carter <colincarter@me.com>
2015-01-24 20:38:42 -0600

    Remove background filters and overlays and replace with pre-filtered background image.  Make primary content overflow scroll on fixed background.  Add transparent blue background to navigation.  Update contact links.  Add initial page content.

---
+++ app/scripts/app.js
@@ -0,0 +1,10 @@
+// Show/Hide page sections
+$(document).ready(function() {
+  $('#hero').hide();
+  $('#about').hide();
+  $('#contact').hide();
+});
+
+$('.js-link-about').click(function() {
+  $('#about').hide();
+});
