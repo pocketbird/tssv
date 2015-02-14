ccc04f4b1988706c892c7821cabd1dc20df021a9
Colin Carter <colincarter@me.com>
2015-02-14 01:25:37 -0600

    Add tour dates.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,25 +21,28 @@ $('.js-link-about').click(function() {
   $('.js-about').show(1000);
   $('.js-blog').hide(1000);
   $('.js-contact').hide(1000);
-  $('.js-tour').hide(1000);
+  $('.js-tour').hide();
 
   $('.js-li-about').addClass('active');
   $('.js-li-contact').removeClass('active');
   $('.js-li-tour').removeClass('active');
+  $('.content-primary').removeClass('content--tour');
 });
 
 $('.js-link-blog').click(function() {
   $('.js-about').hide(1000);
   $('.js-blog').show(1000);
   $('.js-contact').hide(1000);
-  $('.js-tour').hide(1000);
+  $('.js-tour').hide();
+  $('.content-primary').removeClass('content--tour');
 });
 
 $('.js-link-contact').click(function() {
   $('.js-about').hide(1000);
   $('.js-blog').hide(1000);
   $('.js-contact').show(1000);
-  $('.js-tour').hide(1000);
+  $('.js-tour').hide();
+  $('.content-primary').removeClass('content--tour');
 
   $('.js-li-about').removeClass('active');
   $('.js-li-contact').addClass('active');
@@ -47,12 +50,13 @@ $('.js-link-contact').click(function() {
 });
 
 $('.js-link-tour').click(function() {
-  $('.js-about').hide(1000);
-  $('.js-blog').hide(1000);
-  $('.js-contact').hide(1000);
+  $('.js-about').hide();
+  $('.js-blog').hide();
+  $('.js-contact').hide();
   $('.js-tour').show(1000);
 
   $('.js-li-about').removeClass('active');
   $('.js-li-contact').removeClass('active');
   $('.js-li-tour').addClass('active');
+  $('.content-primary').addClass('content--tour');
 });
